26c31886d11513536973979d6bd759e9ef6d1714
Reese2596 <reese2596@student.cptc.edu>
2020-01-15 13:15:38 -0800

    Add project files.

---
+++ EncapsulationPractice-DiceRoll/Die.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPractice_DiceRoll
+{
+    /// <summary>
+    /// Represents a single 6-sided die
+    /// </summary>
+    class Die
+    {
+        //static members get shared across all instances of this class
+        private static Random rand;
+
+        /// <summary>
+        /// Runs once before any die obj is created and initailizes random obj once
+        /// </summary>
+        static Die()
+        {
+            rand = new Random();
+        }
+
+        public Die()
+        {
+            Roll();
+        }
+        /// <summary>
+        /// current value of the die
+        /// </summary>
+        public byte FaceValue { get; private set; } //face value set so cant give value of die in other class.
+        /// <summary>
+        /// die that are held are not rolled
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Generates a new random face value
+        /// sets face value
+        /// returns the generated face value
+        /// </summary>
+        public byte Roll()
+        {
+            //if current die is held return the curr value
+            if (IsHeld)
+            {
+                return FaceValue;
+            }
+            const byte MinValue = 1;
+            const byte MaxValue = 6;
+            //Offset is used for exclusive range Random.Next()
+            const byte Offset = 1;
+            byte newValue = (byte)rand.Next(MinValue, MaxValue + Offset);
+
+            FaceValue = newValue;
+            return newValue;
+        }
+    }
+}
---
+++ EncapsulationPractice-DiceRoll/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPractice_DiceRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Die myDice = new Die();
+            //Console.WriteLine(myDice.FaceValue);
+
+            //myDice.Roll();
+
+            //Console.WriteLine(myDice.FaceValue);
+            Die[] dice = new Die[5]; //Create five die
+            InitDice(dice);
+            PrintDieFaceValues(dice);
+
+            Console.WriteLine("\n\n\nHolding first 3 Die\n\n\n");
+            //Hold the first 3 dice
+            dice[0].IsHeld = true;
+            dice[1].IsHeld = true;
+            dice[2].IsHeld = true;
+            RollAllDie(dice);
+            PrintDieFaceValues(dice);
+
+            Console.ReadKey();
+        }
+
+        private static void RollAllDie(Die[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i].Roll();
+            }
+        }
+
+        private static void PrintDieFaceValues(Die[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                Console.WriteLine(dice[i].FaceValue);//print out the values
+            }
+        }
+
+        /// <summary>
+        /// Initailizes all die in a given array.
+        /// assumes array has been given a length
+        /// </summary>
+        /// <param name="dice"></param>
+        private static void InitDice(Die[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+            }
+        }
+    }
+}
